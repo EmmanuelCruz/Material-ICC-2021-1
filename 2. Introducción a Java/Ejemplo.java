4c6fa5e9b81ffb0fbcda833245b3da6572cfb465
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2021-05-09 16:52:50 -0500

    Introducción a Java

---
+++ 2. Introducción a Java/Ejemplo.java
@@ -0,0 +1,45 @@
+/**
+* Este es un programa de Ejemplo de declaración
+* de variables y uso de operadores.
+* @author Emmanuel Cruz Hernández.
+* @version 1.0 Octubre 2020
+* @since ICC 2021-1
+*/
+public class Ejemplo{
+
+	/* Entero de ejemplo. */
+	public int variable1;
+
+	/** Entero 2 de ejemplo. */
+	public int variable2;
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 4;
+		int c = 1;
+
+		a %= 2;
+
+		double y = 2;
+
+		double yCuadrada = y*y*y;
+
+		double dos = 2;
+
+		boolean booleanTrue = true;
+
+		// Descripciones pequeñas
+
+		/* Que está pasando!
+		dsfdsfdsfsd
+		sdfsdffds
+		sdfdsfds
+		sdfsfdsdsd
+		*/
+
+		// 5/2 = 2, r = 1
+
+		System.out.println(yCuadrada);
+	}
+}
